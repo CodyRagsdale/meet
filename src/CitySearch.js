b08be8eb33a575545f9a398fd25e7bc3b0caf466
CodyRagsdale <119916333+CodyRagsdale@users.noreply.github.com>
2023-06-08 16:21:48 -0400

    Integrate Google API functionality and update CSS

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,11 +1,11 @@
 // src/CitySearch.js
 
 import React, { Component } from "react";
-
 class CitySearch extends Component {
   state = {
     query: "",
     suggestions: [],
+    showSuggestions: undefined,
   };
 
   handleInputChanged = (event) => {
@@ -22,7 +22,10 @@ class CitySearch extends Component {
   handleItemClicked = (suggestion) => {
     this.setState({
       query: suggestion,
+      showSuggestions: false,
     });
+
+    this.props.updateEvents(suggestion);
   };
 
   render() {
@@ -33,8 +36,14 @@ class CitySearch extends Component {
           className="city"
           value={this.state.query}
           onChange={this.handleInputChanged}
+          onFocus={() => {
+            this.setState({ showSuggestions: true });
+          }}
         />
-        <ul className="suggestions">
+        <ul
+          className="suggestions"
+          style={this.state.showSuggestions ? {} : { display: "none" }}
+        >
           {this.state.suggestions.map((suggestion) => (
             <li
               key={suggestion}
@@ -43,7 +52,7 @@ class CitySearch extends Component {
               {suggestion}
             </li>
           ))}
-          <li key="all">
+          <li onClick={() => this.handleItemClicked("all")}>
             <b>See all cities</b>
           </li>
         </ul>
